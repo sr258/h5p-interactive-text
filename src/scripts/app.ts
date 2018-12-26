491903d593b73bb687434e2ee53f3bc4a9eeb8c6
Sebastian Rettig <serettig@posteo.de>
2018-12-26 22:27:23 +0100

    added library semantics in typescript

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,4 +1,5 @@
 import * as $ from 'jquery';
+import { InteractiveTextConfig } from './config';
 
 declare var H5P: any;
 declare var H5PIntegration: any;
@@ -11,20 +12,35 @@ export default class InteractiveText extends (H5P.EventDispatcher as { new(): an
    * @param {string} contentId
    * @param {object} contentData
    */
-  constructor(config: any, contentId: string, contentData: any = {}) {
+  constructor(config: InteractiveTextConfig, contentId: string, contentData: any = {}) {
     super();
-    let username = H5PIntegration.user.name || 'world';
-    this.element = document.createElement('div');
-    this.element.innerText = config.textField.replace('%username', username);
+    const root = document.createElement('div');
+    for (const paragraph of config.content) {
+      const details = document.createElement('details');
+      if (!paragraph.hide)
+        details.setAttribute("open", "open");
+
+      const summary = document.createElement('summary');
+      summary.innerText = "";
+      details.appendChild(summary);
+
+      const p = document.createElement('p');
+      p.innerHTML = paragraph.text;
+      details.appendChild(p);
+
+      root.appendChild(details);
+    }
+
+    this.element = root;
   }
-  
+
   /**
    * Attach library to wrapper
    *
    * @param {jQuery} $wrapper
    */
-  attach = function($wrapper: JQuery) {
+  attach = function ($wrapper: JQuery) {
     $wrapper.get(0).classList.add('h5p-interactive-text');
     $wrapper.get(0).appendChild(this.element);
-  }  
+  }
 }
---
+++ src/scripts/config.ts
@@ -0,0 +1,28 @@
+export class Paragraph {
+  public hide: boolean;
+  public text: string;
+}
+
+export class Dictionary {
+  public name: string;
+  public url: string;
+  public css: string;
+}
+
+export class InteractiveTextConfig {
+  public media?: any;
+  public content: Paragraph[];
+  public source: {
+    author?: string;
+    title?: string;
+    date?: string;
+    url?: string;
+  };
+  public dictionaries: Dictionary[];
+  public behaviour: {
+    freelookup: boolean,
+    defaultdictionary: string,
+    dictionaryopenformat: "popup" | "window" | "linksamewindow" | "linknewwindow",
+    text2speech: "disabled" | string;
+  }
+}
