3e7f8afb0ec2cc0faf506d4dee49126b669a7e9f
Sebastian Rettig <serettig@posteo.de>
2018-12-29 21:19:32 +0100

    parser improvements

--- src/scripts/parser.ts
+++ src/scripts/parser.ts
@@ -1,6 +1,8 @@
 export default class Parser {
   public parse(html: string): string {
     const regex1 = /(\w+)\s*\(=\s*(.*?)\)/g;
-    return html.replace(regex1, '<span class="annotated">$1 <span class="annotation">$2</span></span>');
+    return html.replace(regex1, (substr, arg1, arg2) => {
+      return `<span class="annotated">${arg1.replace(/_/g, " ")} <span class="annotation">${arg2}</span></span>`;
+    });
   }
 }
--- test/parser.spec.ts
+++ test/parser.spec.ts
@@ -8,10 +8,15 @@ describe("Markup parser", () => {
     assert.equal(parser.parse("abc def ghi jkl (= explanation text)"),
       'abc def ghi <span class="annotated">jkl <span class="annotation">explanation text</span></span>');
   });
-  it('should detect the explanation text for "jkl" and replace underscores with whitespaces in \
-   "abc def_ghi_jkl (= explanation text)"', () => {
+  it('should detect the explanation text for "def ghi jkl" and replace underscores with whitespaces in \
+      "abc def_ghi_jkl (= explanation text)"', () => {
     const parser = new Parser();
     assert.equal(parser.parse("abc def_ghi_jkl (= explanation text)"),
       'abc <span class="annotated">def ghi jkl <span class="annotation">explanation text</span></span>');
+  });
+  it('should detect the explanation text for "ghi jkl" in "abc def (ghi jkl) (= explanation text)"', () => {
+    const parser = new Parser();
+    assert.equal(parser.parse("abc def (ghi jkl) (= explanation text)"),
+      'abc def <span class="annotated">ghi jkl <span class="annotation">explanation text</span></span>');
   });
 });
