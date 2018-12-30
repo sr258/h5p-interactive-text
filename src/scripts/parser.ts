44d848bda2759ce5963883fbacc904f5c83494de
Sebastian Rettig <serettig@posteo.de>
2018-12-30 11:08:35 +0100

    parser handles dictionary links

--- src/scripts/parser.ts
+++ src/scripts/parser.ts
@@ -1,8 +1,25 @@
 export default class Parser {
   public parse(html: string): string {
-    const regex1 = /(\w+)\s*\(=\s*(.*?)\)/g;
-    return html.replace(regex1, (substr, arg1, arg2) => {
-      return `<span class="annotated">${arg1.replace(/_/g, " ")} <span class="annotation">${arg2}</span></span>`;
+    html = this.parseInlineAnnotations(html);
+    html = this.parseDictionaryLinks(html);
+    return html;
+  }
+
+  private parseInlineAnnotations(html: string): string {
+    const regex1 = /(\(([(\w\s]+)\)|(\w+))\s*\(\s*=\s*(.*?)\)/g;
+    return html.replace(regex1, (substr, group1, group2, group3, group4) => {
+      const annotatedText = group2 || group1;
+      return `<span class="annotated">${annotatedText
+        .replace(/_/g, " ")} <span class="annotation">${group4}</span></span>`;
+    });
+  }
+
+  private parseDictionaryLinks(html: string): string {
+    const regex1 = /(\(([(\w\s]+)\)|(\w+))\s*\(\s*->\s*(.*?)\)/g;
+    return html.replace(regex1, (substr, group1, group2, group3, group4) => {
+      const annotatedText = group2 || group1;
+      return `<span class="dictionary-linked" data-dictionary-id="${group4}">${annotatedText
+        .replace(/_/g, " ")}</span>`;
     });
   }
 }
--- test/parser.spec.ts
+++ test/parser.spec.ts
@@ -8,15 +8,35 @@ describe("Markup parser", () => {
     assert.equal(parser.parse("abc def ghi jkl (= explanation text)"),
       'abc def ghi <span class="annotated">jkl <span class="annotation">explanation text</span></span>');
   });
+
   it('should detect the explanation text for "def ghi jkl" and replace underscores with whitespaces in \
       "abc def_ghi_jkl (= explanation text)"', () => {
-    const parser = new Parser();
-    assert.equal(parser.parse("abc def_ghi_jkl (= explanation text)"),
-      'abc <span class="annotated">def ghi jkl <span class="annotation">explanation text</span></span>');
-  });
+      const parser = new Parser();
+      assert.equal(parser.parse("abc def_ghi_jkl (= explanation text)"),
+        'abc <span class="annotated">def ghi jkl <span class="annotation">explanation text</span></span>');
+    });
+
   it('should detect the explanation text for "ghi jkl" in "abc def (ghi jkl) (= explanation text)"', () => {
     const parser = new Parser();
     assert.equal(parser.parse("abc def (ghi jkl) (= explanation text)"),
       'abc def <span class="annotated">ghi jkl <span class="annotation">explanation text</span></span>');
+  });
+
+  it('should detect the dictionary link for "jkl" in "abc def ghi jkl (-> dict1)"', () => {
+    const parser = new Parser();
+    assert.equal(parser.parse("abc def ghi jkl (-> dict1)"),
+      'abc def ghi <span class="dictionary-linked" data-dictionary-id="dict1">jkl</span>');
+  });
+
+  it('should detect the dictionary link for "ghi jkl" in "abc def ghi_jkl (-> dict1)"', () => {
+    const parser = new Parser();
+    assert.equal(parser.parse("abc def ghi_jkl (-> dict1)"),
+      'abc def <span class="dictionary-linked" data-dictionary-id="dict1">ghi jkl</span>');
+  });
+
+  it('should detect the dictionary link for "def ghi jkl" in "abc (def ghi jkl) (-> dict1)"', () => {
+    const parser = new Parser();
+    assert.equal(parser.parse("abc (def ghi jkl) (-> dict1)"),
+      'abc <span class="dictionary-linked" data-dictionary-id="dict1">def ghi jkl</span>');
   });
 });
